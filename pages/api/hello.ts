726c71d3ee585d0c4f43fff25d062c3babf03f45
Nikita Volodin <volodin.n@gmail.com>
2021-02-12 16:41:11 -0500

    refactor: switch sample js files to ts

---
+++ pages/api/hello.ts
@@ -0,0 +1,12 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from 'next'
+
+const handler = (
+  req: NextApiRequest,
+  res: NextApiResponse<unknown>
+): void | Promise<void> => {
+  res.status(200).json({ name: 'John Doe' })
+}
+
+export default handler
