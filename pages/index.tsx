ca938bef403882a096d95e69dfad3c6710d37a14
Nikita Volodin <volodin.n@gmail.com>
2021-02-14 18:27:59 -0500

    feat: replace css with emotion+MUI styled components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,67 +1,202 @@
+import type { CardProps } from '@material-ui/core/Card'
+import Card from '@material-ui/core/Card'
+import CardContent from '@material-ui/core/CardContent'
+import { experimentalStyled as styled } from '@material-ui/core/styles'
+import type { TypographyProps } from '@material-ui/core/Typography'
+import Typography from '@material-ui/core/Typography'
 import Head from 'next/head'
-import * as React from 'react'
 
-import styles from '../styles/Home.module.css'
+import type { LinkProps } from '../src/Link'
+import { Link } from '../src/Link'
+
+const Container = styled('div')`
+  min-height: 100vh;
+  padding: 0 0.5rem;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Main = styled('main')`
+  padding: 5rem 0;
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+const Title = styled((p: TypographyProps) => (
+  <Typography variant="h1" {...p} />
+))`
+  line-height: 1.15;
+  font-size: 4rem;
+  font-weight: 500;
+`
+
+const Subtitle = styled(Typography)`
+  line-height: 1.5;
+  font-size: 1.5rem;
+  margin: 1rem 0;
+`
+
+const Code = styled((p: TypographyProps) => (
+  <Typography variant="code" {...p} />
+))`
+  font-size: 1.1rem;
+  padding: 0.75rem;
+  border-radius: 5px;
+  background: #fafafa;
+`
+
+const Grid = styled('div')`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-wrap: wrap;
+  max-width: 800px;
+  margin-top: 3rem;
+
+  @media (max-width: 600px) {
+    width: 100%;
+    flex-direction: column;
+  }
+`
+
+const CardLink = styled((props: LinkProps) => (
+  <Link underline="none" {...props} />
+))`
+  margin: 1rem;
+
+  flex-basis: 45%;
+  text-align: left;
+  color: inherit;
+`
+
+const Tile = styled((p: CardProps) => (
+  <Card {...p} sx={{ boxShadow: 0, ...p.sx }} />
+))`
+  border: 1px solid #eaeaea;
+  border-radius: 10px;
+  transition: color 0.15s ease, border-color 0.15s ease;
+
+  :hover,
+  :focus,
+  :active {
+    color: #0070f3;
+    border-color: #0070f3;
+  }
+`
+
+const TileTitle = styled((p: TypographyProps) => (
+  <Typography variant="h5" gutterBottom {...p} />
+))`
+  margin: 0 0 1rem 0;
+  font-size: 1.5rem;
+  font-weight: 500;
+`
+
+const TileContent = styled(Typography)`
+  margin: 0;
+  font-size: 1.25rem;
+  line-height: 1.5;
+`
+
+const Footer = styled('footer')`
+  width: 100%;
+  height: 100px;
+  border-top: 1px solid #eaeaea;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  a {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  img {
+    margin-left: 0.5rem;
+    height: 1em;
+  }
+`
 
 export default function Home(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <Container>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <Main>
+        <Title>
+          Welcome to <Link href="https://nextjs.org">Next.js!</Link>
+        </Title>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
+        <Subtitle>
+          Get started by editing <Code>pages/index.tsx</Code>
+        </Subtitle>
 
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+        <Grid>
+          <CardLink href="https://nextjs.org/docs">
+            <Tile>
+              <CardContent>
+                <TileTitle>Documentation &rarr;</TileTitle>
+                <TileContent>
+                  Find in-depth information about Next.js features and API.
+                </TileContent>
+              </CardContent>
+            </Tile>
+          </CardLink>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          <CardLink href="https://nextjs.org/learn">
+            <Tile>
+              <CardContent>
+                <TileTitle>Learn &rarr;</TileTitle>
+                <TileContent>
+                  Learn about Next.js in an interactive course with quizzes!
+                </TileContent>
+              </CardContent>
+            </Tile>
+          </CardLink>
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          <CardLink href="https://github.com/vercel/next.js/tree/master/examples">
+            <Tile>
+              <CardContent>
+                <TileTitle>Examples &rarr;</TileTitle>
+                <TileContent>
+                  Discover and deploy boilerplate example Next.js projects.
+                </TileContent>
+              </CardContent>
+            </Tile>
+          </CardLink>
 
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+          <CardLink href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app">
+            <Tile>
+              <CardContent>
+                <TileTitle>Deploy &rarr;</TileTitle>
+                <TileContent>
+                  Instantly deploy your Next.js site to a public URL with
+                  Vercel.
+                </TileContent>
+              </CardContent>
+            </Tile>
+          </CardLink>
+        </Grid>
+      </Main>
 
-      <footer className={styles.footer}>
-        <a
+      <Footer>
+        <Link
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+          Powered by <img src="/vercel.svg" alt="Vercel Logo" />
+        </Link>
+      </Footer>
+    </Container>
   )
 }
--- src/theme.ts
+++ src/theme.ts
@@ -1,3 +1,55 @@
 import { createMuiTheme } from '@material-ui/core'
 
-export const theme = createMuiTheme()
+export const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#0070f3',
+    },
+  },
+  typography: {
+    fontFamily: [
+      '-apple-system',
+      'BlinkMacSystemFont',
+      'Segoe UI',
+      'Roboto',
+      'Oxygen',
+      'Ubuntu',
+      'Cantarell',
+      'Fira Sans',
+      'Droid Sans',
+      'Helvetica Neue',
+      'sans-serif',
+    ].join(', '),
+    code: {
+      fontFamily: [
+        'Menlo',
+        'Monaco',
+        'Lucida Console',
+        'Liberation Mono',
+        'DejaVu Sans Mono',
+        'Bitstream Vera Sans Mono',
+        'Courier New',
+        'monospace',
+      ].join(', '),
+      fontSize: '1.1rem',
+      borderRadius: '5px',
+      padding: '0.75rem',
+    },
+  },
+})
+
+declare module '@material-ui/core/styles' {
+  interface TypographyVariants {
+    code: React.CSSProperties
+  }
+
+  interface TypographyVariantsOptions {
+    code?: React.CSSProperties
+  }
+}
+
+declare module '@material-ui/core/Typography' {
+  interface TypographyPropsVariantOverrides {
+    code: true
+  }
+}
