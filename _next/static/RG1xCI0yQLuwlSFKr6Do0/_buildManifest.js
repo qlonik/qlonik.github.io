7b659e9561e73bfbe26087ff6c3e55abf01718a3
qlonik <volodin.n@gmail.com>
2021-02-14 23:39:54 +0000

    Deploying to gh-pages from @ qlonik/qlonik.github.io@ca938bef403882a096d95e69dfad3c6710d37a14 🚀

---
+++ _next/static/RG1xCI0yQLuwlSFKr6Do0/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-e21ff5236f505b26c1dc.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-df53b83be125e4df4b79.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
