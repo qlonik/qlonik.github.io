0b4c86f5cce43496354fca725d216cebfffab08d
Nikita Volodin <volodin.n@gmail.com>
2021-02-28 21:39:28 -0500

    chore(next): import .mdx with toml frontmatter

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,2 +1,9 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
+
+declare module '*.mdx' {
+  import type { ComponentType } from 'react'
+
+  const MDXComponent: ComponentType<Record<string, unknown>>
+  export default MDXComponent
+}
--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
-module.exports = {
-  reactStrictMode: true,
-  future: {
-    webpack5: true,
+const withPlugins = require('next-compose-plugins')
+
+module.exports = withPlugins(
+  [
+    require('@next/mdx')({
+      extension: /\.mdx$/,
+      options: {
+        remarkPlugins: [[require('remark-frontmatter'), 'toml']],
+        rehypePlugins: [],
+      },
+    }),
+  ],
+  {
+    reactStrictMode: true,
+    future: {
+      webpack5: true,
+    },
   },
-}
+)
